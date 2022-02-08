ac1f514fc49abbbc0866e943b55aedb39e1b0e87
Rahul Madan <madan.rahul9@gmail.com>
2022-02-09 03:47:57 +0530

    changing path on successful login completed

--- src/App.js
+++ src/App.js
@@ -2,18 +2,30 @@ import './App.css';
 import {LoginPage} from "./MyComponents/LoginPage";
 import {Navbar} from "./MyComponents/Navbar";
 import {useState} from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
     const [aadhaarNumber, setAadhaarNumber] = useState("")
     const [password, setPassword] = useState("")
 
 
-  return (
-    <>
-      <Navbar/>
-      <LoginPage aadhaarNumber={aadhaarNumber} setAadhaarNumber={setAadhaarNumber} password={password} setPassword={setPassword}/>
-    </>
-  );
+    return (
+        <>
+            <Router>
+                <Navbar/>
+                <Routes>
+                    <Route exact path="/"
+                           element={<LoginPage aadhaarNumber={aadhaarNumber}
+                                               setAadhaarNumber={setAadhaarNumber}
+                                               password={password}
+                                               setPassword={setPassword}/>}>
+                    </Route>
+                    <Route exact path="/about" element={<Navbar/>}>
+                    </Route>
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default App;
--- src/MyComponents/LoginPage.js
+++ src/MyComponents/LoginPage.js
@@ -1,13 +1,20 @@
-import React from 'react'
 import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
 export const LoginPage = (props) => {
 
+    let navigate = useNavigate();
+    const routeChange = (path) =>{
+        navigate(path);
+    }
 
     const loginSubmit = (e)=> {
         e.preventDefault()
-        axios.get("http://localhost:3001/retrieve", {}).then((result)=>{
+        axios.post("http://localhost:8000/login", {'aadhaarNumber': props.aadhaarNumber, 'password': props.password}).then((result)=>{
             console.log(result)
+            if(result.data.statusCode === 0){
+                routeChange('/about')
+            }
         })
 }
 
@@ -18,13 +25,13 @@ export const LoginPage = (props) => {
                 <div className="mb-3">
                     <label htmlFor="aadhaarNumberInput" className="form-label">Enter Aadhaar Number</label>
                     <input type="number" value={props.aadhaarNumber} onChange={(e)=>{props.setAadhaarNumber(e.target.value)}} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                        <div id="emailHelp" className="form-text">We'll never share your Aadhaar Number with anyone else.</div>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
                     <input type="password" value={props.password} onChange={(e)=>{props.setPassword(e.target.value)}} className="form-control" id="exampleInputPassword1"/>
                 </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
+                <button type="submit" className="btn btn-primary">Log in</button>
             </form>
         </div>
     )
