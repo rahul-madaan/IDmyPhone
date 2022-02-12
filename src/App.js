49ddbc31a487d38d5bc6a2af525ac875d44099ba
Rahul Madan <madan.rahul9@gmail.com>
2022-02-12 19:43:12 +0530

    merged all functionalities

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {Navbar} from "./MyComponents/Navbar";
 import {useState} from "react";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import {LoginRegisterNavbar} from "./MyComponents/LoginRegisterNavbar";
+import {RegisterPage} from "./MyComponents/RegisterPage";
+import {HomePage} from "./MyComponents/HomePage";
 
 function App() {
     const [aadhaarNumber, setAadhaarNumber] = useState("")
@@ -15,17 +17,12 @@ function App() {
             <Router>
                 <Routes>
                     <Route exact path="/login"
-                           element={<LoginPage aadhaarNumber={aadhaarNumber}
+                           element={<><LoginRegisterNavbar/>
+                               <LoginPage aadhaarNumber={aadhaarNumber}
                                                setAadhaarNumber={setAadhaarNumber}
                                                password={password}
-                                               setPassword={setPassword}/>}/>
-                    <Route exact path="/home" element={<HomePage/>}/>
-                           element={<>
-                               <LoginRegisterNavbar/>
-                               <LoginPage aadhaarNumber={aadhaarNumber}
-                                          setAadhaarNumber={setAadhaarNumber}
-                                          password={password}
-                                          setPassword={setPassword}/></>}/>
+                                               setPassword={setPassword}/></>}/>
+                    <Route exact path="/home" element={<><Navbar/><HomePage/></>}/>
                     <Route exact path="/register"
                            element={<>
                                <LoginRegisterNavbar/>
