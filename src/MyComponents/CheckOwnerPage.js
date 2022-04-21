fce652ec975a49f78327cd5f867f05713499a16f
Rahul Madan <madan.rahul9@gmail.com>
2022-04-21 20:34:05 +0530

    added User Profile heading in container 1, left

--- src/MyComponents/CheckOwnerPage.js
+++ src/MyComponents/CheckOwnerPage.js
@@ -118,6 +118,7 @@ export const CheckOwnerPage = (props) => {
             <div className="container rounded bg-white ">
                 <div className="row">
                     <div className="col-md-3">
+                        <h3>User Profile</h3>
                         <div className="d-flex flex-column  "><img
                             className="rounded-circle mt-5" width="250px"
                             src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"/>
