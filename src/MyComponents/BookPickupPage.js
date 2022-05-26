0e819b391fb01f3f6cadcaa6185ef32d1d5ab82e
Rahul Madan <madan.rahul9@gmail.com>
2022-05-26 20:30:44 +0530

    removed unused import from BookPickupPage

--- src/MyComponents/BookPickupPage.js
+++ src/MyComponents/BookPickupPage.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import {useNavigate} from "react-router-dom";
-import React, {useEffect, useLayoutEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import PhoneImage from "../MyPhotos/image-removebg-preview.png"
 
 export const BookPickupPage = (props) => {
