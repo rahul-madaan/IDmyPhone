73691253fd2731cebf1c50805efec9628ac7e498
Rahul Madan <madan.rahul9@gmail.com>
2022-06-06 21:53:01 +0530

    removed unused imports from HomePage.js

--- src/MyComponents/HomePage.js
+++ src/MyComponents/HomePage.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import {useNavigate} from "react-router-dom";
 import React, {useEffect, useLayoutEffect, useState} from "react";
 import {HomeTableContent} from "./HomeTableContent";
 
