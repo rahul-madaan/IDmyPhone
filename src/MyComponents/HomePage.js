371de77ff616b8a67c47e0cd8d20d260af3b4994
Rahul Madan <madan.rahul9@gmail.com>
2022-06-09 22:51:08 +0530

    removed unused import useEffect from HomePage.js file

--- src/MyComponents/HomePage.js
+++ src/MyComponents/HomePage.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, {useEffect, useLayoutEffect, useState} from "react";
+import React, {useLayoutEffect, useState} from "react";
 import {HomeTableContent} from "./HomeTableContent";
 
 
